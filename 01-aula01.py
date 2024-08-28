a9db80c4cd11d8769413bfe9bc81e7a2c7883f24
JoaoPinto7-design <joazinho123k4@gmail.com>
2024-08-28 17:23:12 -0300

    Add files via upload

---
+++ 01-aula01.py
@@ -0,0 +1,6 @@
+# Aqui eu posso adicionar comentários
+# Continuar na linha de baixo
+print("Eduardo") # comentário a linha do código
+print('Eduardo')
+print('Eduardo "Dudu" Hernandes')
+print("Eduardo 'Dudu' Hernandes")
