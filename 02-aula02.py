0136ca3b0f197d0171c8d12fda4f29ecbba93d97
JoaoPinto7-design <joazinho123k4@gmail.com>
2024-09-11 15:16:28 -0300

    Add files via upload

---
+++ 02-aula02.py
@@ -0,0 +1,9 @@
+# atribuição de variaveis
+nome = "Joao"
+idade = 15
+altura = 1.80
+
+# exibir mensagem na tela
+print(" nome: ", nome, " idade: ", idade, " altura: ", altura)
+# formatação com f-string
+print(f"nome: {nome} idade: {idade} altura: {altura}")
