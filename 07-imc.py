4dfc48a32fd29ba140a58f5be174a4eb5398a375
JoaoPinto7-design <joazinho123k4@gmail.com>
2024-09-12 18:28:46 -0300

    Add files via upload

---
+++ 07-imc.py
@@ -0,0 +1,17 @@
+print("Programa para cálculo de IMC")
+peso = float(input("Digite o seu peso (em kg): "))
+altura = float(input("Digite a sua altura (em metros): "))
+imc = peso / (altura**2)
+
+print(f"Seu IMC é (imc:.2f)")   # .2f é uasdo
+# para mostrar apenas duas casas decimais
+
+if (imc < 18.5):
+    print("Abaixo do peso")
+elif (18.5 <= imc < 25):
+    print("Peso normal.")
+elif (25 <= imc < 35):
+    print("Sobrepeso.")
+else:
+    print("Obesidade.")
+    
